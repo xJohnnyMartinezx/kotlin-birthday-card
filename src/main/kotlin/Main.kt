6fdb16a377958f34aaddead10838d139e50a9d88
Johnny Martinez <mrtnz.jn@gmail.com>
2023-05-05 09:14:27 -0500

    added finishing touches

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+
+
+
 //fun main() {
 //    val border = "`-._,-'"
 //    val timesToRepeat = 4
@@ -13,55 +16,57 @@
 //    println()
 //}
 
-//********** CAKE BUILDING *********
-fun main(){
+    //********** CAKE BUILDING *********
+    fun main() {
 
-    val age = 24;
-    val layers = 5;
+        val age = 24;
+        val layers = 5;
 
-    //    CALLING printCakeTop FUNCTION
-    printCakeCandles(age);
+        //    CALLING printCakeTop FUNCTION
+        printCakeCandles(age);
 
-    //    CALLING printCakeTop FUNCTION
-    printCakeTop(age);
+        //    CALLING printCakeTop FUNCTION
+        printCakeTop(age);
 
-    //    CALLING printCakeBottom FUNCTION
-    printCakeBottom(age, layers);
+        //    CALLING printCakeBottom FUNCTION
+        printCakeBottom(age, layers);
 
-}
-//******** CAKE TOP FUNC *********
-fun printCakeTop(age: Int){
-    repeat(age + 2){
-        print("=");
     }
-    println();
-}
 
-//******** CANDLES FUNC ********
-fun printCakeCandles(age: Int){
-    print(" ");
-    repeat(age){
-        print(",");
-    }
-    println();
-    print(" ");
-
-    repeat(age){
-        print("|");
-    }
-    println();
-}
-
-//******* CAKE BOTTOM ******
-fun printCakeBottom(age: Int, layers: Int){
-
-    repeat(layers){
+    //******** CAKE TOP FUNC *********
+    fun printCakeTop(age: Int) {
         repeat(age + 2) {
-            print("@");
+            print("=");
         }
         println();
     }
 
-}
+    //******** CANDLES FUNC ********
+    fun printCakeCandles(age: Int) {
+        print(" ");
+        repeat(age) {
+            print(",");
+        }
+        println();
+        print(" ");
+
+        repeat(age) {
+            print("|");
+        }
+        println();
+    }
+
+    //******* CAKE BOTTOM ******
+    fun printCakeBottom(age: Int, layers: Int) {
+
+        repeat(layers) {
+            repeat(age + 2) {
+                print("@");
+            }
+            println();
+        }
+
+    }
+
 
 
