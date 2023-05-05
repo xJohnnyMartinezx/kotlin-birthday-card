e9f1092ea2b76176d2375fb10f22793317cec073
Johnny <mrtnz.jn@gmail.com>
2023-05-04 19:15:16 -0500

    finished creating cake portion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,67 @@
-fun main() {
-    val border = "`-._,-'"
-    val timesToRepeat = 4
-    printBorder(border, timesToRepeat)
-    println("  Happy Birthday, Jhansi!")
-    printBorder(border, timesToRepeat)
+//fun main() {
+//    val border = "`-._,-'"
+//    val timesToRepeat = 4
+//    printBorder(border, timesToRepeat)
+//    println("  Happy Birthday, Jhansi!")
+//    printBorder(border, timesToRepeat)
+//}
+//
+//fun printBorder(border: String, timesToRepeat: Int) {
+//    repeat(timesToRepeat) {
+//        print(border)
+//    }
+//    println()
+//}
+
+//********** CAKE BUILDING *********
+fun main(){
+
+    val age = 24;
+    val layers = 5;
+
+    //    CALLING printCakeTop FUNCTION
+    printCakeCandles(age);
+
+    //    CALLING printCakeTop FUNCTION
+    printCakeTop(age);
+
+    //    CALLING printCakeBottom FUNCTION
+    printCakeBottom(age, layers);
+
+}
+//******** CAKE TOP FUNC *********
+fun printCakeTop(age: Int){
+    repeat(age + 2){
+        print("=");
+    }
+    println();
 }
 
-fun printBorder(border: String, timesToRepeat: Int) {
-    repeat(timesToRepeat) {
-        print(border)
+//******** CANDLES FUNC ********
+fun printCakeCandles(age: Int){
+    print(" ");
+    repeat(age){
+        print(",");
     }
-    println()
+    println();
+    print(" ");
+
+    repeat(age){
+        print("|");
+    }
+    println();
 }
+
+//******* CAKE BOTTOM ******
+fun printCakeBottom(age: Int, layers: Int){
+
+    repeat(layers){
+        repeat(age + 2) {
+            print("@");
+        }
+        println();
+    }
+
+}
+
+
